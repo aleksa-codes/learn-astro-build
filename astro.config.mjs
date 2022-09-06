c7de9f8ff0333c36f8ad826ccfdfa258ef4b3361
aleksa-stojsic <stojsic.aleksa@gmail.com>
2022-09-06 14:24:50 +0200

    Feat: replace google font with fontsource

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://aleksastojsic.github.io',
-  base: '/first-astro-site/',
+  base: '/first-astro-site',
   integrations: [tailwind()]
 });
