a76abd4f675ff26adb54691439c0f7505971457b
aleksa-stojsic <stojsic.aleksa@gmail.com>
2022-09-06 15:32:35 +0200

    Fix: logo showing

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://aleksastojsic.github.io',
-  base: '/first-astro-site',
+  // site: 'https://aleksastojsic.github.io',
+  // base: '/first-astro-site',
   integrations: [tailwind(), image()]
 });
